70e855a23c004818bfc66152a67829c286554c98
Ajay Kumar <60890279+Ajayacme339@users.noreply.github.com>
2025-11-25 12:41:48 -0500

    added code for ollma genapp

---
+++ BUILD_SIMPLE_GENAI_APP_USING_OLLAMA/genai_ollma_app1.py
@@ -0,0 +1,39 @@
+import os
+from langchain_core.output_parsers import StrOutputParser
+import streamlit as st
+from dotenv import load_dotenv
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+load_dotenv()
+
+# LangSmith Tracking
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT")
+
+# Prompt Template
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant. Please respond to the question asked."),
+        ("user", "Question: {question}")
+    ]
+)
+
+# Streamlit UI
+st.title("Langchain Demo with Gemma Model")
+text_input = st.text_input("What questions would you like to ask?")
+
+# LLM
+llm = OllamaLLM(model="gemma:2b")
+
+# Correct parser instance
+parser = StrOutputParser()
+
+# Correct chain order → prompt → LLM → parser
+chain = prompt | llm | parser
+
+# Run chain
+if text_input:
+    result = chain.invoke({"question": text_input})
+    st.write(result)
